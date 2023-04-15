3b8562838403d6624b96cb50d2fb876005adc663
Alparslan <alparslanaydgn02@gmail.com>
2023-04-15 19:15:12 +0300

    Simple changes

--- app/src/main/java/com/example/mymobilapplication/MainActivity.java
+++ app/src/main/java/com/example/mymobilapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mymobilapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 editTextEnterName.setText("");
             }
         });
+
 
     }
 
